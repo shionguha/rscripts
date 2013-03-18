7345f79df5f44bd656d7c788c4960563f5b20ea3
Shion <shionguha@gmail.com>
2013-03-18 15:32:12 -0300

    Update testargs.R
    Updated description with sample command line

--- testargs.R
+++ testargs.R
@@ -1,3 +1,6 @@
+#This is a sample R script to test passing arguments from the command line to R and outputting them.
+# A sample command line is as follows: R --slave --no-save --no-restore --no-environ --silent --args arg1=abc < testargs.R
+# This is inspiration gained from:  http://www.perlmonks.org/?node_id=644967
 cat("-- reading arguments\n", sep = "");
 cmd_args = commandArgs();
 for (arg in cmd_args) cat("  ", arg, "\n", sep="");
