a6c90265c9da75281226039d2e20694a486f4291
Shion <shionguha@gmail.com>
2014-03-02 18:15:31 -0500

    minor code edit

--- kmeanssampler.R
+++ kmeanssampler.R
@@ -2,7 +2,7 @@
 library(mclust)
 
 #randomly simulating gaussian mixture model
-simdata = sim(modelName = "V",parameters = list(pro = c(0.95, 0.05),mean = c(0, 0),variance = list(modelName = "V",                                                     d = 1,G = 2,sigmasq = c(0, 36))),n = 10)
+simdata = sim(modelName = "V",parameters = list(pro = c(0.95, 0.05),mean = c(0, 0),variance = list(modelName = "V",d = 1,G = 2,sigmasq = c(0, 36))),n = 10)
 
 #fitting k means
 fitk<-kmeans(simdata,3)
